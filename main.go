c677396049e7534e211dd2d2a61db6f84069cf86
chien.nm1 <chien.nm1@teko.vn>
2024-04-04 00:55:51 +0700

    commit

--- main.go
+++ main.go
@@ -2,21 +2,46 @@ package main
 
 import "fmt"
 
+const (
+	Installment = "Installment"
+	Credit      = "Credit"
+	Online      = "Online"
+)
+
 func main() {
-	a, b, c := 100.0, 4.0, 5.0
-	fmt.Printf(checkTriangleType(a, b, c))
+	fmt.Print(needPayAmount(10000, 1000, Credit))
 }
 
-func checkTriangleType(a, b, c float64) string {
-	if a+b > c && a+c > b && b+c > a {
-		if a == b && b == c {
-			return "Tam giác đều"
-		} else if a == b || b == c || c == a {
-			return "Tam giác cân"
-		} else {
-			return "Tam giác thường"
-		}
-	} else {
-		return "Không phải tam giác"
+func needPayAmount(realAmount int32, paidAmount int32, method string) int32 {
+	if realAmount < paidAmount {
+		return -1
 	}
+	amount := realAmount - paidAmount
+
+	var res int32
+	switch method {
+	case Installment:
+		res = int32(float32(amount+2) * 0.3)
+		break
+	case Credit:
+		res = 0
+		break
+	case Online:
+		res = amount
+		break
+	default:
+		res = -1
+		break
+	}
+	return res
+}
+
+func calc(n int) int {
+	ans := 0
+	if n < 0 {
+		ans = 0
+	} else {
+		ans = int(n * (n + 1) * (2*n + 1) / 6)
+	}
+	return ans
 }
--- main_test.go
+++ main_test.go
@@ -1,68 +1,242 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
-type Triangle struct {
-	a float64
-	b float64
-	c float64
+type Payment struct {
+	n      int32
+	m      int32
+	method string
 }
 
-type TestCase struct {
+type TestCasePayment struct {
 	name     string
-	triangle Triangle
-	expected string
+	payment  Payment
+	expected int32
 }
 
-func TestCheckTriangleType(t *testing.T) {
-	testCases := []TestCase{
+type TestCaseSum struct {
+	name     string
+	n        int
+	expected int
+}
+
+func TestCheckPayAmount(t *testing.T) {
+	testCases := []TestCasePayment{
 		{
-			name: "tam-giac-deu",
-			triangle: Triangle{
-				a: 5,
-				b: 5,
-				c: 5,
+			name: "id00",
+			payment: Payment{
+				n:      100000,
+				m:      50000,
+				method: "Credit",
 			},
-			expected: "Tam giác đều",
+			expected: 0,
 		},
 		{
-			name: "tam-giac-can",
-			triangle: Triangle{
-				a: 5,
-				b: 5,
-				c: 6,
+			name: "id01",
+			payment: Payment{
+				n:      100000,
+				m:      50000,
+				method: "Installment",
 			},
-			expected: "Tam giác cân",
+			expected: 15000,
 		},
 		{
-			name: "tam-giac-thuong",
-			triangle: Triangle{
-				a: 4,
-				b: 5,
-				c: 6,
+			name: "id02",
+			payment: Payment{
+				n:      100000,
+				m:      50000,
+				method: "Online",
 			},
-			expected: "Tam giác thường",
+			expected: 50000,
 		},
 		{
-			name: "khong-phai-tam-giac",
-			triangle: Triangle{
-				a: 100,
-				b: 5,
-				c: 6,
+			name: "id03",
+			payment: Payment{
+				n:      100000,
+				m:      1,
+				method: "Credit",
 			},
-			expected: "Không phải tam giác",
+			expected: 0,
+		},
+		{
+			name: "id04",
+			payment: Payment{
+				n:      100000,
+				m:      1,
+				method: "Installment",
+			},
+			expected: 30000,
+		},
+		{
+			name: "id05",
+			payment: Payment{
+				n:      100000,
+				m:      1,
+				method: "Online",
+			},
+			expected: 99999,
+		},
+		{
+			name: "id06",
+			payment: Payment{
+				n:      100000,
+				m:      2,
+				method: "Credit",
+			},
+			expected: 0,
+		},
+		{
+			name: "id07",
+			payment: Payment{
+				n:      100000,
+				m:      2,
+				method: "Installment",
+			},
+			expected: 30000,
+		},
+		{
+			name: "id08",
+			payment: Payment{
+				n:      100000,
+				m:      2,
+				method: "Online",
+			},
+			expected: 99998,
+		},
+		{
+			name: "id09",
+			payment: Payment{
+				n:      1,
+				m:      2,
+				method: "Credit",
+			},
+			expected: -1,
+		},
+		{
+			name: "id10",
+			payment: Payment{
+				n:      100,
+				m:      1,
+				method: "Credit",
+			},
+			expected: 0,
+		},
+		{
+			name: "id11",
+			payment: Payment{
+				n:      101,
+				m:      1,
+				method: "Installment",
+			},
+			expected: 30,
+		},
+		{
+			name: "id12",
+			payment: Payment{
+				n:      100,
+				m:      1,
+				method: "Online",
+			},
+			expected: 99,
+		},
+		{
+			name: "id13",
+			payment: Payment{
+				n:      100,
+				m:      2,
+				method: "NoMethod",
+			},
+			expected: -1,
+		},
+		{
+			name: "id14",
+			payment: Payment{
+				n:      1,
+				m:      2,
+				method: "Credit",
+			},
+			expected: -1,
+		},
+		{
+			name: "id15",
+			payment: Payment{
+				n:      101,
+				m:      1,
+				method: "Installment",
+			},
+			expected: 30,
+		},
+		{
+			name: "id16",
+			payment: Payment{
+				n:      100,
+				m:      1,
+				method: "Credit",
+			},
+			expected: 0,
+		},
+		{
+			name: "id17",
+			payment: Payment{
+				n:      100,
+				m:      1,
+				method: "Online",
+			},
+			expected: 99,
+		},
+		{
+			name: "id13",
+			payment: Payment{
+				n:      100,
+				m:      1,
+				method: "NoMethod",
+			},
+			expected: -1,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			if tc.expected == checkTriangleType(tc.triangle.a, tc.triangle.b, tc.triangle.c) {
-				fmt.Printf("pass")
-			} else {
-				fmt.Printf("failed")
+			if tc.expected != needPayAmount(tc.payment.n, tc.payment.m, tc.payment.method) {
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestCalc(t *testing.T) {
+	testCases := []TestCaseSum{
+		{
+			name:     "id0",
+			n:        0,
+			expected: 0,
+		},
+		{
+			name:     "id1",
+			n:        3,
+			expected: 14,
+		},
+		{
+			name:     "id2",
+			n:        0,
+			expected: 0,
+		},
+		{
+			name:     "id3",
+			n:        0,
+			expected: 0,
+		},
+		{
+			name:     "id4",
+			n:        4,
+			expected: 30,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.expected != calc(tc.n) {
 				t.Fail()
 			}
 		})
